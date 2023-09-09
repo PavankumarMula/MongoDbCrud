a6deea8f9e11f9b66af10eb8f4205fa0ebb6ac30
PavankumarMula <pavanpappy7@gmail.com>
2023-09-09 14:38:37 +0530

    Last Deploy

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-const path = require('path');
-const envfile=require('dotenv').config({path:"./.env"}).parsed;
-
-const mongourl=envfile.MONGO_URI;
-const port = envfile.PORT_NUMBER;
+require("dotenv").config();
 
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-// routers 
-const userRouter = require('./routes/userRouter');
+// routers
+const userRouter = require("./routes/userRouter");
 const expenseRouter = require("./routes/expenseRouter");
 
 // user router
 app.use(userRouter);
 app.use(expenseRouter);
 
-// const PORT = process.env.PORT_NUMBER || 4000;
-// const mongoUrl = process.env.MONGO_URI;
+// env variables
+const PORT = process.env.PORT_NUMBER || 4000;
+const userName = process.env.USER;
+const password = process.env.PASSWORD;
+const database = process.env.DB_NAME;
 
 // connecting to mongodb database
-mongoose.connect(mongourl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(result => {
-  console.log(`MongoDb is connected<<<<<<<<`);
-  app.listen(port);
-})
-.catch(err => console.log(err));
+mongoose
+  .connect(
+    `mongodb+srv://${userName}:${password}@cluster0.j383jen.mongodb.net/${database}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((result) => {
+    console.log(`MongoDb is connected<<<<<<<<`);
+    app.listen(PORT);
+  })
+  .catch((err) => console.log(err));
